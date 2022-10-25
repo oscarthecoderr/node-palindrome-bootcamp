2dcf1baefd5e4100619daaf6883aba6dfe049066
oscarthecoderr <dev.oscarrios@gmail.com>
2022-10-25 01:22:37 -0400

    got js and html completed. css need some work. node.js needs to be doubled check

--- main.js
+++ main.js
@@ -1,8 +1,96 @@
 
 
-function palindrome(text){
+document.querySelector(.button).addEventListener('click',palindrome)
 
 
-  let reversedText = text.toLowerCase().split('').reverse.join('')
-  return text === reversedText
+function palindrome(){
+  const text = document.querySelector('input').value
+  fetch(`/api?text=${text}`)
+  .then(res => res.json)
+  .then((data) =>{
+   console.log(data)
+   document.querySelector('.answer').textContent = data.result
+  })
 }
+
+// let reversedText = text.toLowerCase().split('').reverse.join('')
+//   return text === reversedText
+
+// const http = require('http');
+// const fs = require('fs')
+// const url = require('url');
+// const querystring = require('querystring');
+// const figlet = require('figlet')
+
+// const server = http.createServer(function(req, res) {
+//   const page = url.parse(req.url).pathname;
+//   const params = querystring.parse(url.parse(req.url).query);
+//   console.log(page);
+//   if (page == '/') {
+//     fs.readFile('index.html', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+//   else if (page == '/otherpage') {
+//     fs.readFile('otherpage.html', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+//   else if (page == '/otherotherpage') {
+//     fs.readFile('otherotherpage.html', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+//   else if (page == '/api') {
+//     let reversedText = text.toLowerCase().split('').reverse().join('')
+//     if('text' in params){
+//       if(params['text']== reversedText){
+//         res.writeHead(200, {'Content-Type': 'application/json'});
+//         const objToJson = {
+//           result: true,
+         
+//         }
+//         res.end(JSON.stringify(objToJson));
+//       }//student = leon
+//       else if(params['text'] !== reversedText){
+//         let reversedText = text.toLowerCase().split('').reverse().join('')
+//         res.writeHead(200, {'Content-Type': 'application/json'});
+//         const objToJson = {
+//           result: false,
+//         }
+//         res.end(JSON.stringify(objToJson));
+//       }//student != leon
+//     }//student if
+//   }//else if
+//   else if (page == '/css/style.css'){
+//     fs.readFile('css/style.css', function(err, data) {
+//       res.write(data);
+//       res.end();
+//     });
+//   }else if (page == '/js/main.js'){
+//     fs.readFile('js/main.js', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/javascript'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }else{
+//     figlet('404!!', function(err, data)
+//      {
+//       if (err) {
+//           console.log('Something went wrong...');
+//           console.dir(err);
+//           return;
+//       }
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+// });
+
+// server.listen(8000);
